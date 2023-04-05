9b07bddd82e518ce27bb17390411a26a78712a69
Erzal Fahrezy Primanda <fahrezyprimanda@gmail.com>
2023-04-05 10:33:20 +0700

    kolam lele

---
+++ kolamLele.cs
@@ -0,0 +1,12 @@
+public class KolamLele
+{
+    public void show()
+    {
+        Console.WriteLine("        INI KOLAM LELE       ");
+        Console.WriteLine("         =============       ");
+        Console.WriteLine("     ====             ====");
+        Console.WriteLine("   ===                  ===");
+        Console.WriteLine("     ===              ====");
+        Console.WriteLine("         =============       ");
+    }
+}
