80a75e054c00dac6c8f64a4d71d2b3e18eae42e1
Wiwit Annisa Puspaningsari <wiwit.annisa14@gmail.com>
2023-04-05 11:00:17 +0700

    program panah lele

---
+++ BunuhLele.cs
@@ -0,0 +1,14 @@
+public class KolamLele
+{
+    public void show()
+    {
+        Console.WriteLine("        INI PANAH BUAT BUNUH LELE       ");
+        Console.WriteLine("                            / |");
+        Console.WriteLine("      /  |                 / /|");
+        Console.WriteLine("     /   |-----------------\ \|");
+        Console.WriteLine("     \   |-----------------\ \|");
+        Console.WriteLine("      \  |                 \ \|");
+        Console.WriteLine("                            \ \|");
+        
+    }
+}
